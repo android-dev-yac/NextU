187fbf511779f39293cc33a2d2f59f416583ee6f
ManosPlus <manosplus5@gmail.com>
2015-12-06 00:15:30 -0500

    90% evalucion final de DOM y BOM falta mayor

--- 2_BOMDOM_EvaluacionFinal/js/main.js
+++ 2_BOMDOM_EvaluacionFinal/js/main.js
@@ -5,6 +5,7 @@ var btnMostrarRegistrar = document.getElementById("btn-mostraregistrar");
 var btnMostrarMayor = document.getElementById("btn-mostrarmayor");
 var btnMostrarMenor = document.getElementById("btn-mostrarmenor");
 
+// Boton Registar Estudiante
 btnRegistrar.addEventListener("click", function () {
 
     var formCodigo = document.getElementById("formCodigo").value;
@@ -13,7 +14,7 @@ btnRegistrar.addEventListener("click", function () {
 
     if (formCodigo==null || formCodigo=="", formNombre==null || formNombre=="", formNotas==null || formNotas=="")
     {
-        alert("Please Fill All Required Field");
+        alert("Por favor rellene Campo Obligatorio");
         return false;
     }
 
@@ -23,7 +24,15 @@ btnRegistrar.addEventListener("click", function () {
         "notas": formNotas
     };
     estudiantes.push(estudianteNuevo);
+
+    var clearField = document.getElementsByName('question')
+    for(var i = 0; i < clearField.length; i++){
+        clearField[i].value = '';
+    }
+    å
 });
+
+/*=============== Boton Mostrar Registrar ===============*/
 btnMostrarRegistrar.addEventListener("click", function () {
     var resultado = "";
     resultado += '<table>' + '<thead><tr><th>Código</th><th>Nombre</th><th>Nota</th></tr></thead>';
@@ -34,4 +43,38 @@ btnMostrarRegistrar.addEventListener("click", function () {
     }
     resultado += '</table>'
     document.getElementById('tablEstudiante').innerHTML = resultado;
+
+
+});
+
+/*=============== Boton Mostrar Notas Mayores ===============*/
+btnMostrarMayor.addEventListener("click", function () {
+    var promMay = "";
+    promMay += '<table>' + '<thead><tr><th>Código</th><th>Nombre</th><th>Nota</th></tr></thead>';
+    for (var i = 0; i < estudiantes.length; i++) {
+        var xSub = Number(estudiantes[i].notas).toFixed(1);
+        if(xSub >= 4.0) {
+            promMay += '<tbody><tr><td>' + estudiantes[i].codigo + '</td>';
+            promMay += '<td>' + estudiantes[i].nombre + '</td>';
+            promMay += '<td>' + xSub + '</td></tr></tbody>';
+        }
+    }
+    promMay += '</table>'
+    document.getElementById('tablEstudiante').innerHTML = promMay;
+});
+
+/*=============== Boton Mostrar Notas Mayores ===============*/
+btnMostrarMayor.addEventListener("click", function () {
+    var promMay = "";
+    promMay += '<table>' + '<thead><tr><th>Código</th><th>Nombre</th><th>Nota</th></tr></thead>';
+    for (var i = 0; i < estudiantes.length; i++) {
+        var xSub = Number(estudiantes[i].notas).toFixed(1);
+        if(xSub >= 4.0) {
+            promMay += '<tbody><tr><td>' + estudiantes[i].codigo + '</td>';
+            promMay += '<td>' + estudiantes[i].nombre + '</td>';
+            promMay += '<td>' + xSub + '</td></tr></tbody>';
+        }
+    }
+    promMay += '</table>'
+    document.getElementById('tablEstudiante').innerHTML = promMay;
 });
