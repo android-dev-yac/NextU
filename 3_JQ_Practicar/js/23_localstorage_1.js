47bf1b842e33770a1811b23601d0e560ecf66901
ManosPlus <manosplus5@gmail.com>
2016-01-20 10:07:00 -0500

    Subire folder 1 y bom

--- 3_JQ_Practicar/js/23_localstorage_1.js
+++ 3_JQ_Practicar/js/23_localstorage_1.js
@@ -1,5 +1,39 @@
-$(function(){
+$(document).ready(function(){
 
+    $('.btn1').click(function(){
 
+        var id = $('#campId').val();
+        var nombre = $('#campNombre').val();
+        var apellido = $('#campApellido').val();
+
+        var persona =  {
+            id: id,
+            nombre: nombre,
+            apellido: apellido
+        };
+
+        localStorage.setItem(id, JSON.stringify(persona));
+
+        listarPersona();
+
+        $('#campId').val("");
+        $('#campNombre').val("");
+        $('#campApellido').val("");
+
+    });
+
+    function listarPersona() {
+
+        var elements = '<h3>Lista Persona:</h3>';
+
+        for (var i = 0; i < localStorage.length; i++) {
+            var clave = localStorage.key(i);
+            var persona = $.parseJSON(localStorage.getItem(clave));
+
+            elements +=  persona.id + '-' + persona.nombre + '-' + persona.apellido;
+        }
+        $('.p1').html(elements);
+    }
+    listarPersona();
 
 });
